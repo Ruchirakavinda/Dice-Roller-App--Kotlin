0f74c938428980fde40ee069b5b16d2d54cd1ff4
Ruchirakavinda <ruchirakvnd@gmail.com>
2022-01-30 23:34:37 +0530

    action bar

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,9 +2,14 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.view.menu.MenuView
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +21,31 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT).show()
         }
 
+        val toolbar : Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        toolbar.setNavigationOnClickListener {
+            Toast.makeText(this, "Main Menu Appeared", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu01, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val emptydice : ImageView = findViewById(R.id.dice_image)
+        var itemview = item.itemId
+        when(itemview){
+            R.id.reset -> emptydice.setImageResource(R.drawable.empty_dice),Toast.makeText(applicationContext,"Dice Reseted", Toast.LENGTH_LONG).show()
+            R.id.user -> Toast.makeText(applicationContext,"User Details", Toast.LENGTH_LONG).show()
+            R.id.settings -> Toast.makeText(applicationContext,"Settings", Toast.LENGTH_LONG).show()
+            R.id.more -> Toast.makeText(applicationContext,"More Options", Toast.LENGTH_LONG).show()
+        }
+
+        return false
     }
 
     private fun diceRoller() {
